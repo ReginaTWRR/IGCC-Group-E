1605e988727fa5cc2eae74afdce5f95effc433e5
Regero <reginaeeteng@gmail.com>
2026-09-09 17:57:16 +0900

    setting up toolbar UI, to continue.

---
+++ Assets/Scripts/Inventory/System/InventoryInputHandler.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum ScrollMovement
+{
+    None,
+    Upwards,
+    Downwards
+}
+
+public class InventoryInputHandler
+{
+    // Keep a list of all the keybinds that will be used to select a box in the toolbar
+    // ツールバー内のボックスを選択するために使用するすべてのキーバインドのリストを保持してください。
+    readonly Key[] toolbarKeys =
+    {
+        Key.Digit1,
+        Key.Digit2,
+        Key.Digit3,
+        Key.Digit4,
+        Key.Digit5,
+        Key.Digit6,
+        Key.Digit7,
+        Key.Digit8,
+        Key.Digit9,
+        Key.Digit0,
+        Key.Minus,
+        Key.Equals
+    };
+
+    // Store the selected key index
+    // 選択されたキーのインデックスを保存します
+    int selectedKeyIndex = 0;
+
+    public int GetKeySelection()
+    {
+        for (int i = 0; i < toolbarKeys.Length; ++i)
+        {
+            if (Keyboard.current[toolbarKeys[i]].wasPressedThisFrame)
+            {
+                selectedKeyIndex = i;
+                return selectedKeyIndex;
+            }
+        }
+
+        return selectedKeyIndex;
+    }
+
+    public ScrollMovement CheckMouseScroll()
+    {
+        Vector2 mouseScroll = Mouse.current.scroll.ReadValue();
+        
+        if (mouseScroll.y > 0f)
+        {
+            return ScrollMovement.Upwards;
+        }
+        else if (mouseScroll.y < 0f)
+        {
+            return ScrollMovement.Downwards;
+        }
+
+        return ScrollMovement.None;
+    }
+}
---
+++ Assets/Scripts/Inventory/UI/SelectionOutline.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionOutline : Singleton<SelectionOutline>
+{
+    [Header("Selection Outline")]
+    [SerializeField] Image outlineImage;
+
+    
+}
