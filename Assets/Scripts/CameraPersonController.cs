39f79410c275f00e20d5a42fc29ecea9202e5c6f
it252157 <it252157@trident.ac.jp>
2026-09-09 23:32:56 +0900

    Camera View Switching Behavior　視点カメラ切り替え挙動

---
+++ Assets/Scripts/CameraPersonController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+// Script for changing the camera viewpoint　カメラの視点変更に関するスクリプト
+public class CameraPersonController : MonoBehaviour
+{
+    [Header("Reference point for the viewpoint 「視点の基準点」")]
+
+    // First person fixed position　1人称視点固定位置
+    [SerializeField] private Transform firstPersonAnchor;
+
+    // Third person fixed position　3人称視点固定位置
+    [SerializeField] private Transform thistPersonAnchor;
+
+
+    [Header("System 「設定」")]
+
+    // Interpolation speed　補間速度
+    [SerializeField] private float transitionSpeed = 3.0f;
+
+    // Target fixation　標的固定
+    private Transform targetAnchor;
+
+    // Flag management for the first-person perspective　1人称視点かのフラグ管理
+    private bool isFirstPerson = true;
+
+
+    void Start()
+    {
+        // Set the initial viewpoint to first person　初期視点を1人称視点に設定
+        targetAnchor = firstPersonAnchor;
+
+        // Align the initial position and initial rotation　初期位置と初期回転を合わせる
+        transform.position = targetAnchor.position;
+        transform.rotation = targetAnchor.rotation;
+    }
+
+    void LateUpdate()
+    {
+        if (targetAnchor == null) return;
+
+        // Interpolated movement toward the target　ターゲットに向かって補間移動
+        transform.position = Vector3.Lerp(transform.position, targetAnchor.position, Time.deltaTime * transitionSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetAnchor.rotation, Time.deltaTime * transitionSpeed);
+    }
+
+    // Function to switch the viewpoint　視点を切り替える関数
+    public void TogglePerspective()
+    {
+        // Toggle the flag　フラグを切り替える
+        isFirstPerson = !isFirstPerson;
+
+        // Switch targets　ターゲットを切り替える
+        targetAnchor = isFirstPerson ? firstPersonAnchor : thistPersonAnchor;
+    }
+}
---
+++ Assets/Scripts/PlayerChangeController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Script for switching players　プレイヤーの切り替えに関するスクリプト
+public class PlayerChangeController : MonoBehaviour
+{
+    [Header("Object 「オブジェクト」")]
+
+    // Human Object　人間オブジェクト
+    [SerializeField] private GameObject humanObject;
+
+    // Ghost Object　幽霊オブジェクト
+    [SerializeField] private GameObject ghostObject;
+
+    // Camera　カメラ
+    [SerializeField] private Camera mainCamera;
+
+
+    [Header("Input 「入力」")]
+
+    // Input Device　入力デバイス
+    [SerializeField] private PlayerInput inputDevice;
+
+
+    //Function to change to a ghost　プレイヤーを幽霊に変更する関数
+    void OnChangeGhost()
+    {
+        // Set object active state　オブジェクトのアクティブを設定
+        humanObject.SetActive(false);
+        ghostObject.SetActive(true);
+
+        // Set input device to Ghost　入力デバイスを幽霊に設定
+        inputDevice.SwitchCurrentActionMap("Ghost");
+
+        // Switch perspectives　視点を切り替える
+        mainCamera.GetComponent<CameraPersonController>().TogglePerspective();
+    }
+
+    //Function to change to a human　プレイヤーを人間に変更する関数
+    void OnChangeHuman()
+    {
+        // Set object active state　オブジェクトのアクティブを設定
+        humanObject.SetActive(true);
+        ghostObject.SetActive(false);
+
+        // Set input device to Human　入力デバイスを人間に設定
+        inputDevice.SwitchCurrentActionMap("Human");
+
+        // Switch perspectives　視点を切り替える
+        mainCamera.GetComponent<CameraPersonController>().TogglePerspective();
+    }
+}
